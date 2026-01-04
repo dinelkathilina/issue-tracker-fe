e7543278ffb5eceb40f53718922f9b23b742370f
dinelkathilina <dinelkathilina3@gmail.com>
2026-01-04 15:43:11 +0530

    feat: Implement base layout structure with NavBar and Outlet

--- issue-tracker-fe/src/layouts/MainLayout.tsx
+++ issue-tracker-fe/src/layouts/MainLayout.tsx
@@ -5,7 +5,7 @@ export default function MainLayout() {
   return (
     <div className="min-h-screen bg-content1 text-foreground bg-gradient-to-r from-slate-900 to-slate-700">
       <NavBar />
-      <main className="container mx-auto px-6 py-8 flex justify-center">
+      <main className="container mx-auto px-6 py-2 flex justify-center">
         <Outlet/>
       </main>
     </div>
